7d093466107f9d1d1db5e488dc98b2cf0a82e273
Franci Penov <francip@gmail.com>
2015-05-23 20:25:59 -0700

    More Pi tweaks

--- color_test.sh
+++ color_test.sh
@@ -51,43 +51,43 @@ EOF
     eval "$(_bash_color_definitions)"
     eval "$(_bash_os_definitions)"
 
+    echo -e $COLOR_RED               'Red              '$COLOR_NONE
+    echo -e $COLOR_GREEN             'Green            '$COLOR_NONE
+    echo -e $COLOR_YELLOW            'Yellow           '$COLOR_NONE
+    echo -e $COLOR_BLUE              'Blue             '$COLOR_NONE
+    echo -e $COLOR_MAGENTA           'Magenta          '$COLOR_NONE
+    echo -e $COLOR_CYAN              'Cyan             '$COLOR_NONE
+    echo -e $COLOR_WHITE             'White            '$COLOR_NONE
+    echo -e $COLOR_NONE              'None             '$COLOR_NONE
+
+    echo -e $COLOR_RED_BOLD          'Red bold         '$COLOR_NONE
+    echo -e $COLOR_GREEN_BOLD        'Green bold       '$COLOR_NONE
+    echo -e $COLOR_YELLOW_BOLD       'Yellow bold      '$COLOR_NONE
+    echo -e $COLOR_BLUE_BOLD         'Blue bold        '$COLOR_NONE
+    echo -e $COLOR_MAGENTA_BOLD      'Magenta bold     '$COLOR_NONE
+    echo -e $COLOR_CYAN_BOLD         'Cyan bold        '$COLOR_NONE
+    echo -e $COLOR_WHITE_BOLD        'White bold       '$COLOR_NONE
+    echo -e $COLOR_BOLD              'Bold             '$COLOR_NONE
+
+    echo -e $COLOR_RED_UNDERLINE     'Red underline    '$COLOR_NONE
+    echo -e $COLOR_GREEN_UNDERLINE   'Green underline  '$COLOR_NONE
+    echo -e $COLOR_YELLOW_UNDERLINE  'Yellow underline '$COLOR_NONE
+    echo -e $COLOR_BLUE_UNDERLINE    'Blue underline   '$COLOR_NONE
+    echo -e $COLOR_MAGENTA_UNDERLINE 'Magenta underline'$COLOR_NONE
+    echo -e $COLOR_CYAN_UNDERLINE    'Cyan underline   '$COLOR_NONE
+    echo -e $COLOR_WHITE_UNDERLINE   'White underline  '$COLOR_NONE
+    echo -e $COLOR_UNDERLINE         'Underline        '$COLOR_NONE
+
+    echo -e $COLOR_RED_INVERT        'Red invert       '$COLOR_NONE
+    echo -e $COLOR_GREEN_INVERT      'Green invert     '$COLOR_NONE
+    echo -e $COLOR_YELLOW_INVERT     'Yellow invert    '$COLOR_NONE
+    echo -e $COLOR_BLUE_INVERT       'Blue invert      '$COLOR_NONE
+    echo -e $COLOR_MAGENTA_INVERT    'Magenta invert   '$COLOR_NONE
+    echo -e $COLOR_CYAN_INVERT       'Cyan invert      '$COLOR_NONE
+    echo -e $COLOR_WHITE_INVERT      'White invert     '$COLOR_NONE
+    echo -e $COLOR_INVERT            'Invert           '$COLOR_NONE
 }
 
-__install_main "$@"SH_INTERACTIVE ] && echo -e $COLOR_RED               'Red              '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_GREEN             'Green            '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_YELLOW            'Yellow           '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_BLUE              'Blue             '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_MAGENTA           'Magenta          '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_CYAN              'Cyan             '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_WHITE             'White            '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_NONE              'None             '$COLOR_NONE
-
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_RED_BOLD          'Red bold         '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_GREEN_BOLD        'Green bold       '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_YELLOW_BOLD       'Yellow bold      '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_BLUE_BOLD         'Blue bold        '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_MAGENTA_BOLD      'Magenta bold     '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_CYAN_BOLD         'Cyan bold        '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_WHITE_BOLD        'White bold       '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_BOLD              'Bold             '$COLOR_NONE
-
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_RED_UNDERLINE     'Red underline    '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_GREEN_UNDERLINE   'Green underline  '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_YELLOW_UNDERLINE  'Yellow underline '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_BLUE_UNDERLINE    'Blue underline   '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_MAGENTA_UNDERLINE 'Magenta underline'$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_CYAN_UNDERLINE    'Cyan underline   '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_WHITE_UNDERLINE   'White underline  '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_UNDERLINE         'Underline        '$COLOR_NONE
-
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_RED_INVERT        'Red invert       '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_GREEN_INVERT      'Green invert     '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_YELLOW_INVERT     'Yellow invert    '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_BLUE_INVERT       'Blue invert      '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_MAGENTA_INVERT    'Magenta invert   '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_CYAN_INVERT       'Cyan invert      '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_WHITE_INVERT      'White invert     '$COLOR_NONE
-    [ $BASH_INTERACTIVE ] && echo -e $COLOR_INVERT            'Invert           '$COLOR_NONE
-
-unset __install_main
+__color_test_main "$@"
+unset __color_test_main
 

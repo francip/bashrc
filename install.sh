d1a98952154f2a99a99acfa4dc4411a787627fe3
Franci Penov <francip@gmail.com>
2016-12-20 22:15:51 -0800

    Typo

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ EOF
             if ln -s $BASH_SOURCE_DIR/$BASH_SOURCE_FILE $TEMPFILE >/dev/null 2>&1; then
                 rm $TEMPFILE
             else
-                echo -e $COLOR_YELLOW'WARNING:'$COLOR_NONE' Open new Bash windows as administrator, set '$COLOR_CYAN'export $HOME='$HOME$COLOR_NONE
+                echo -e $COLOR_YELLOW'WARNING:'$COLOR_NONE' Open new Bash windows as administrator, set '$COLOR_CYAN'export HOME='$HOME$COLOR_NONE
                 echo -e 'and rerun the installer.'
 		echo
 		exit
